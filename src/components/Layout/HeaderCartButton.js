4ce9cfffb8d3c4837813e5e24b9a85b3e3a51ce1
Alan Grądecki <alangradecki@wp.pl>
2021-08-22 18:50:43 +0200

    HeaderCartButton component has implemented

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,14 @@
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
 const HeaderCartButton = (props) => {
   return (
-    <button>
-      <span></span>
-      <span></span>
-      <span></span>
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
     </button>
   );
 };
