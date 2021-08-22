7abaf7ecb4bea6b08c25e55d76db10aafdcf5982
Alan Grądecki <alangradecki@wp.pl>
2021-08-22 19:13:22 +0200

    Meals, AvailableMeals components has implemented

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,3 +1,14 @@
-const Meals = () => {};
+import { Fragment } from "react";
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
 
 export default Meals;
